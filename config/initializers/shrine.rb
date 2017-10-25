ad9a376106d3759e22d4555c4458921cd595ea97
Carlos Orellana <carlos.cw2106@gmail.com>
2017-10-25 16:43:11 -0600

    Configured route for upload endpoint. Configured shrine.rb with the necessary plugins and AS3 config. Renamed application.scss

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,10 +1,21 @@
-require "shrine"
+require "shrine/storage/s3"
 require "shrine/storage/file_system"
 
+
+s3_options = {
+  access_key_id:      ENV['ACCESS_KEY_ID'],
+  secret_access_key:  ENV['SECRET_ACCESS_KEY'],
+  region:             ENV['BUCKET_REGION'],
+  bucket:             ENV['BUCKET_NAME'],
+  endpoint:           ENV["AWS_HOST"],
+}
+
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+  store: Shrine::Storage::S3.new(prefix: "store", **s3_options),
 }
 
 Shrine.plugin :activerecord
+Shrine.plugin :direct_upload
+Shrine.plugin :restore_cached_data
 Shrine.plugin :cached_attachment_data # for forms
